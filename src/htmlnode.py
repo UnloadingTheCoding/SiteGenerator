5e1e6dbe5f1fb90a3b6fc4d8119f4544e8243e9f
Alfredo Boron <alron1984@gmail.com>
2024-05-24 23:37:54 -0700

    HTMLNode v1 done w/unittest

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,5 +1,24 @@
 
 class HTMLNode:
 
-    def __init__(self,str tag, value,list children,dict props):
-        pass
+    def __init__(self,tag: str = None, value=None, children: list = None, props: dict = None):
+        self.tag = tag
+        self.value = value
+        self.children = children
+        self.props = props
+
+    def to_html(self):
+        raise NotImplementedError
+
+    def props_to_html(self):
+        attribute = ""
+        for pro in self.props:
+            attribute += f"{pro}=\"{self.props[pro]}\" "
+        return attribute
+
+    def __repr__(self):
+        rep = f"tag={self.tag}, value={self.value} children="
+        for child in self.children:
+            rep += f"{child.tag} "
+        rep += f"props={self.props_to_html}"
+
