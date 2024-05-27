8ca257fdc2ef454134617057c0cd1ceb7db95abc
Alfredo Boron <alron1984@gmail.com>
2024-05-26 23:46:28 -0700

    added leafnode and unittests

---
+++ src/leafnode.py
@@ -0,0 +1,22 @@
+
+from htmlnode import HTMLNode
+
+class LeafNode(HTMLNode):
+
+    def __init__(self, value: str, tag: str = None, props = None):
+        super().__init__(tag=tag, value=value, props=props)
+
+    def to_html(self):
+        
+        if self.value is None:
+            raise ValueError("leaf missing value parameter")
+
+        if self.tag is None:
+            return self.value
+
+        if self.props:
+            return f"<{self.tag} {self.props_to_html()}>{self.value}</{self.tag}>"
+        return f"<{self.tag}>{self.value}</{self.tag}>"
+
+    def __repr__(self):
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
---
+++ src/test_leafnode.py
@@ -0,0 +1,27 @@
+import unittest
+
+from leafnode import LeafNode
+
+
+class TestLeafNode(unittest.TestCase):
+
+    def test_no_tag(self):
+        test_leaf = LeafNode("This is test text")
+        self.assertEqual(test_leaf.to_html(), "This is test text")
+
+    def test_no_value(self):
+        test_leaf = LeafNode(None,"p")
+        with self.assertRaises(ValueError):
+            test_leaf.to_html()
+
+    def test_no_props_format(self):
+        test_leaf = LeafNode("Watching Dune part one", "p")
+        self.assertEqual(test_leaf.to_html(), "<p>Watching Dune part one</p>")
+
+    def test_with_props_format(self):
+        test_leaf = LeafNode("Lets watch Dune part two", "a", {"href": "https://www.dune2movielink.org"})
+        self.assertEqual(test_leaf.to_html(), "<a href=\"https://www.dune2movielink.org\" >Lets watch Dune part two</a>")
+
+
+if __name__ == "__main__":
+    unittest.main()
