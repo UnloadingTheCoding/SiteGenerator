3a15b6926a5111696d609b8caaf8dafa2ec04d70
Alfredo Boron <alron1984@gmail.com>
2024-05-29 07:19:10 -0700

    textnode update

--- src/textnode.py
+++ src/textnode.py
@@ -1,4 +1,4 @@
-
+from leafnode import LeafNode
 class TextNode:
 
     def __init__(self, text, text_type, url=None):
@@ -6,15 +6,27 @@ class TextNode:
         self.text_type = text_type
         self.url = url
 
-
     def __eq__(self, other):
         
         if self.text == other.text and self.text_type == other.text_type and self.url == other.url:
             return True
         return False
 
-
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type}, {self.url})"
 
-
+    def text_node_to_html_node(text_node):
+        if text_node.text_type not in ("text", "bold", "italic", "code", "link", "image"):
+            raise Exception("Invalid text_type")
+        if text_node.text_type == "text":
+            return LeafNode(None, text_node.text)
+        if text_node.text_type == "bold":
+            return LeafNode("b", text_node.text)
+        if text_node.text_type == "italic":
+            return LeafNode("i", text_node.text)
+        if text_node.text_type == "code":
+            return LeafNode("code", text_node.text)
+        if text_node.text_type == "link":
+            return LeafNode("a", text_node.text_type, {"href": text_node.url})
+        if text_node.text_type == "image":
+            return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
